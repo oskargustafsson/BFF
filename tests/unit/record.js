2c2f47c438bfcfd386e7bef121f1cabddd294052
Oskar Gustafsson <oskargustafsson88@gmail.com>
2015-04-19 22:11:55 +0200

    Add Record unit tests

--- tests/unit/record.js
+++ tests/unit/record.js
@@ -7,44 +7,75 @@ define(function (require) {
 
   registerSuite(function () {
     var Record;
-    var record;
 
     return {
 
       name: 'Model',
 
-      'setup': function () {
-        Record = factory.create({
-          name: undefined, // no restrictions on this property
-          id: {
-            type: 'number', // specifying a type will force a type check on set
-          },
-          color: {
-            forbiddenValues: [ null, undefined ], // Forbidding undefined is like saying that this field is required
-            defaultValue: 'green',
-          },
-          race: {
-            type: 'string',
-            defaultValue: 'human',
-            setter: function (val) { return val.toLowerCase(); }
-          },
-          avatarUrl: {
-            getter: function () { return 'assets/' + this.race + '_' + this.color + '.png'; },
-            setter: false,
-          },
-        });
+      'type checking': {
+
+        'throws an error if the passed value is of the wrong type': function () {
+            Record = factory.create({
+              race: { type: 'string', },
+            });
+            expect(new Record({ race: 'human' }).race).to.equal('human');
+            expect(function () { new Record({ race: 4 }); }).to.throw();
+        },
+
+        'allows values to be unset and undefined': function () {
+            Record = factory.create({
+              race: { type: 'string', },
+            });
+            expect(new Record().race).to.equal(undefined);
+            expect(new Record({ race: undefined }).race).to.equal(undefined);
+        },
+
       },
 
-      'default values are assigned': function () {
-        record = new Record();
-        expect(record.race).to.equal('human');
+      'forbidden value checking': {
+
+        'throws an error when a forbidden value is passed': function () {
+          Record = factory.create({
+            race: { type: 'string', forbiddenValues: [ undefined, null ] },
+          });
+          expect(function () { new Record(); }).to.throw();
+          expect(function () { new Record({ race: undefined }); }).to.throw();
+          expect(function () { new Record({ race: null }); }).to.throw();
+          expect(new Record({ race: 'human' }).race).to.equal('human');
+        },
+
       },
 
-      'default values can be overridden': function () {
-        record = new Record({
-          race: 'squirrel',
-        });
-        expect(record.race).to.equal('squirrel');
+      'default values': {
+
+        'are assigned': function () {
+          Record = factory.create({
+            race: { defaultValue: 'human', },
+          });
+          expect(new Record().race).to.equal('human');
+        },
+
+        'can be overridden': function () {
+          Record = factory.create({
+            race: { defaultValue: 'human', },
+          });
+          expect(new Record({ race: 'squirrel' }).race).to.equal('squirrel');
+        },
+
+        'throws an error when the wrong type is passed': function () {
+          Record = factory.create({
+            race: { type: 'number', defaultValue: 'human', },
+          });
+          expect(function () { new Record(); }).to.throw();
+        },
+
+        'does not throw an error when the specified type is passed': function () {
+          Record = factory.create({
+            race: { type: 'string', defaultValue: 'human', },
+          });
+          expect(new Record().race).to.equal('human');
+        },
+
       },
 
     };
