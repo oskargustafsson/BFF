0d2b7d20b1b9b4bebd7c1cae2ac5a11bcd72a363
Oskar Gustafsson <oskargustafsson88@gmail.com>
2015-05-01 16:48:30 +0000

    Add Event emitter unit tests

--- tests/unit/event-emitter.js
+++ tests/unit/event-emitter.js
@@ -6,7 +6,6 @@ define(function (require) {
   var sinon = require('node_modules/sinon/pkg/sinon');
   var sinonChai = require('node_modules/sinon-chai/lib/sinon-chai');
   var expect = require('intern/chai!expect');
-  var should = require('intern/chai!should');
 
   var eventEmitter = require('src/event-emitter');
   var mixin = require('src/mixin');
@@ -23,6 +22,7 @@ define(function (require) {
         var emitter = mixin({}, eventEmitter);
         emitter.addEventListener('kwanzaa', function () {});
         emitter.addEventListener('kwanzaa', function () {});
+        expect(emitter.__listeners.kwanzaa.length).to.equal(2);
       },
 
       'can not add the same listener twice': function () {
@@ -34,13 +34,84 @@ define(function (require) {
         }).to.throw();
       },
 
-      'calls all callbacks registered to an event': function () {
+      'can remove all event listeners': function () {
         var emitter = mixin({}, eventEmitter);
         var callback = sinon.spy();
+        var callback2 = sinon.spy();
+
         emitter.addEventListener('kwanzaa', callback);
+        emitter.addEventListener('kwanzaa', callback2);
+        expect(emitter.__listeners.kwanzaa.length).to.equal(2);
+
+        emitter.emit('kwanzaa');
+        expect(callback).have.been.calledOnce;
+        expect(callback2).have.been.calledOnce;
+
+        emitter.removeEventListener('kwanzaa');
+        expect(emitter.__listeners.kwanzaa).to.equal(undefined);
+
+        emitter.emit('kwanzaa');
+        expect(callback).have.been.calledOnce;
+        expect(callback2).have.been.calledOnce;
+      },
+
+      'can remove a specific event listener': function () {
+        var emitter = mixin({}, eventEmitter);
+        var callback = sinon.spy();
+        var callback2 = sinon.spy();
+
+        emitter.addEventListener('kwanzaa', callback);
+        emitter.addEventListener('kwanzaa', callback2);
+
+        emitter.emit('kwanzaa');
+        expect(callback).have.been.calledOnce;
+        expect(callback2).have.been.calledOnce;
+
+        emitter.removeEventListener('kwanzaa', callback);
+        expect(emitter.__listeners.kwanzaa.length).to.equal(1);
+
+        emitter.emit('kwanzaa');
+        expect(callback).have.been.calledOnce;
+        expect(callback2).have.been.calledTwice;
+      },
+
+      'calls all callbacks registered to an event': function () {
+        var emitter = mixin({}, eventEmitter);
+
+        var callback = sinon.spy();
+        var callback2 = sinon.spy();
+
+        emitter.addEventListener('kwanzaa', callback);
+        emitter.addEventListener('kwanzaa', callback2);
+
         emitter.emit('kwanzaa', [ 'firstArg', 'secondArg' ]);
+
         expect(callback).have.been.calledOnce;
         expect(callback).to.have.been.calledWith('firstArg', 'secondArg');
+
+        expect(callback2).have.been.calledOnce;
+        expect(callback2).to.have.been.calledWith('firstArg', 'secondArg');
+      },
+
+      'calls callbacks in the order they were registered': function () {
+        var emitter = mixin({}, eventEmitter);
+
+        var callback = sinon.spy();
+        var callback2 = sinon.spy();
+
+        emitter.addEventListener('kwanzaa', callback);
+        emitter.addEventListener('kwanzaa', callback2);
+
+        emitter.emit('kwanzaa');
+
+        expect(callback).to.have.been.calledBefore(callback2);
+
+        emitter.removeEventListener('kwanzaa', callback);
+        emitter.addEventListener('kwanzaa', callback);
+
+        emitter.emit('kwanzaa');
+
+        expect(callback2).to.have.been.calledBefore(callback);
       },
 
     };
