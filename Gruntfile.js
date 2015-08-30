abcbe2387fc2b97852c4b1981c957af0bde21562
Oskar Gustafsson <oskargustafsson88@gmail.com>
2015-08-30 17:13:35 +0200

    Use Docstrap tempalte for docs

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,6 +9,8 @@ module.exports = function (grunt) {
         src: [ 'src/*.js' ],
         options: {
           destination: 'doc',
+          template: 'node_modules/grunt-jsdoc/node_modules/ink-docstrap/template',
+          configure: 'jsdoc.conf.json',
         },
       },
     },
